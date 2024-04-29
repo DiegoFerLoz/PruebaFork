db1c572cfbf79911a4eeb035a4d42695e03b7025
Diego <diegofernandezl2001@gmail.com>
2024-04-29 13:56:20 +0200

    Modificacion

---
+++ Factorial.java
@@ -0,0 +1,25 @@
+package primeraEvaluacion;
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// Pide un número y calcula la suma de los factoriales hasta ese numero (incluido).
+		Scanner src = new Scanner(System.in);
+		System.out.print("Dame un número para calcular factoriales: ");
+		int numero = src.nextInt();
+		int suma = 0;
+		int factor;
+		src.close();
+		
+		for (int i=1; i<=numero; i++) {
+			factor = 1;
+			for (int j=1; j<=i; j++) {
+				factor = factor * j;
+			}
+			System.out.println("El factorial de "+i+" es: "+ factor);
+			suma = suma + factor;
+		}
+		System.out.println("La suma total es: "+suma);
+	}
+}
